e5e365f887dae8a618a7242685a46fd10e0c155e
desuche <uchedestinyx@gmail.com>
2023-10-25 21:14:31 +0800

    Fixed requirements

--- functions/average.py
+++ functions/average.py
@@ -5,9 +5,9 @@ Description: Calculate the average of some numbers.
 
 Difficulty: Easy
 
-Task: Write a function that takes a list of three numbers as input and returns their average.
+Task: Write a function that takes a list of numbers as input and returns their average.
 
-Input: A list of three numbers (integer or float).
+Input: A list of numbers (integer or float).
 
 Output: Float.
 
